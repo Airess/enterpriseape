cbfd3284fa505b832e928b65508e44be535b6484
Airess <beautebargains@gmail.com>
2014-09-20 13:47:34 -0400

    new branch dropdown

--- app/models/company.rb
+++ app/models/company.rb
@@ -10,7 +10,7 @@ class Company < ActiveRecord::Base
 		CSV.generate do |csv|
 			csv << column_names
 			all.each do |company|
-				csv << company.attributes.values_at(*column_names)
+			csv << company.attributes.values_at(*column_names)
 			end
 		end
 	end
